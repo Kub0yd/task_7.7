301944ff9689e3b9e42da8a861627bd5daa7a149
Kub0yd <vs_egorov@mail.ru>
2022-11-28 17:25:58 +0300

    did function task

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,7 +1,15 @@
 
 window.onload = function()
-{
+{   
     const initPerson = personGenerator.getPerson();
+    let day = "";
+    let mounth = "";
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
-};
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('surnameOutput').innerText = initPerson.surname;
+    (initPerson.day < 10) ? (day = '0' + initPerson.day) : (day = initPerson.day); //если день меньше 10 добавляем 0 для читабельности
+    (initPerson.mounth < 10) ? (mounth = '0' + initPerson.mounth) : (mounth = initPerson.mounth);
+    document.getElementById('birthYearOutput').innerText = day + '.' + mounth + '.' + initPerson.year;
+}; 
 
+document.getElementById("button-clear").onclick()
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -35,6 +35,21 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александра",
+            "id_2": "Мария",
+            "id_3": "Ольга",
+            "id_4": "Татьяна",
+            "id_5": "Юлия",
+            "id_6": "Ксения",
+            "id_7": "Эмилия",
+            "id_8": "Елена",
+            "id_9": "Элла",
+            "id_10": "Ярослава"
+        }
+    }`,
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -48,23 +63,58 @@ const personGenerator = {
     },
 
     randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
-
+        if (this.person.gender ==  personGenerator.GENDER_MALE) {
+           return this.randomValue(this.firstNameMaleJson); 
+        }else {
+            return this.randomValue(this.firstNameFemaleJson); 
+        }
     },
-
 
      randomSurname: function() {
 
         return this.randomValue(this.surnameJson);
 
     },
-
+    //Получаем случайный пол
+    randomGender: function () {
+        const number = Math.round(Math.random());
+        const randomGender = (number === 1) ? personGenerator.GENDER_MALE : personGenerator.GENDER_FEMALE;
+        return randomGender;
+    },
+    // получакм случайный день
+    randomDateDay:function () {
+        let day;
+        const mounth = this.person.mounth;
+        if  (mounth == 1 || mounth == 3 || mounth == 4 || mounth == 7 || mounth == 8 || mounth == 10 || mounth == 12){ //Проверка на месяц, в котором 31 день
+            day = this.randomIntNumber(31, 1);
+        } else if (mounth == 2) {
+            (this.person.year % 4 == 0) ? (day = this.randomIntNumber(29, 1)) : (day = this.randomIntNumber(28, 1)); //Проверка на високосную дату
+        }else { //месяц в котором 30 дней 
+            day = this.randomIntNumber(30, 1);
+        }
+        return day;
+    },
+    //Получаем случайное число месяца
+    randomDateMounth:function () {
+        return this.randomIntNumber(12, 1);
+    },
+    //Получаем случайный год рождения
+    randomDateYear:function () {
+        return this.randomIntNumber(2022, 1930);
+    },
 
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        (this.person.gender == personGenerator.GENDER_MALE) ?        
+            (this.person.surname = this.randomSurname()) : (this.person.surname = this.randomSurname() + "а") ;
+        this.person.year = this.randomDateYear();
+        this.person.mounth = this.randomDateMounth();
+        this.person.day = this.randomDateDay();
+        console.log( this.person.year);
+        console.log(typeof(this.person.day));
+        console.log(typeof(this.person.mounth));
         return this.person;
     }
 };
